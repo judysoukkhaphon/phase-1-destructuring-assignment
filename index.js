6e685c37480b8b734288864d3ad88886ae745136
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-01-17 11:23:40 -0500

    use destructuring operators to create variables

--- index.js
+++ index.js
@@ -28,21 +28,68 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const cow = {
+  sound: "moo",
+  name: "bessie",
+  color: "blackAndWhite"
+}
+/*
+let {moo} = 'cow';
+let {neigh} = 'horse';
+let {baa} = 'sheep';
+let {oink} = 'pig';
+let {cluck} = 'chicken';
+*/
 
+const moo = "cow";
+const neigh = "horse";
+const baa = "sheep";
+const oink = "pig";
+const cluck = "chicken";
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
+const bessie = "cow";
+const dolly = "sheep";
+const babe = "pig";
+const little = "chicken";
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const blackAndWhite = "cow";
+const black = "sheep";
+const pink = "pig";
+
 
 // Arrays
+const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+let [red, orange, yellow, green, blue, indigo, violet] = rainbow;
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+let [r, o, y, g, b, indg,v] = rainbow;
+
+
 
 // Objects
-
+const character = ["Miss Piggy", "pink", "Never Before, Never Again", "Cast member of The Muppet Show", "Kermit"];
 // 7. Use destructuring to assign all variables using the keys as the variable names
+let [muppetName, color, song, job, partner] = character;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const character2 = ["Moving Right Along", "I Hope That Something Better Comes Along", "Host of The Muppet Show", "Miss Piggy"];
+
+let [song2, song4, nestedJob, nestedPartner] = character2;
+
+
+
+
+
+
+
+
+
+
+
+
+
